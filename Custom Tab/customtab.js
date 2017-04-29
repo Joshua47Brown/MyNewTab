b42e98e3e890630af90c632063cc99aedcdf6576
Joshua Brown <joshua47brown@gmail.com>
2017-04-29 23:10:25 +0100

    Beginning Of Theme Functionality
    Syncs with chrome storage.

--- Custom Tab/customtab.js
+++ Custom Tab/customtab.js
@@ -1,6 +1,7 @@
 $(document).ready(function($) {
 
     var taskArray;
+    var page;
     var ellipseButtonSize = "70px";
     var $greeting = $("#greeting");
     var $taskInput = $("#task-input");
@@ -10,7 +11,34 @@ $(document).ready(function($) {
     var $taskList = $("#task-list");
     var $cancelInput = $("#cancel-input");
     var $leftArrow = $("#left-arrow");
-    var $rightArrow = $("#right-arrow")
+    var $rightArrow = $("#right-arrow");
+
+    chrome.storage.sync.get('pageNum', function(p) {
+        switch (p.pageNum) {
+            case -1:
+                SetThemeOne();
+                break;
+            case 0:
+                SetThemeDefault();
+                break;
+            case 1:
+                SetThemeTwo();
+                break;
+        }
+    });
+
+    function SetThemeDefault() {
+        ChangeCSS("customtabstyle.css", 0);
+    }
+    
+    function SetThemeOne() {
+        ChangeCSS("theme-one.css", 0);
+    }
+
+    function SetThemeTwo() {
+        ChangeCSS("theme-two.css", 0);
+    }
+
 
     chrome.storage.sync.get('value', function(c) {
         taskArray = c.value;
@@ -58,9 +86,8 @@ $(document).ready(function($) {
     });
 
     $taskList.on('click', '#checkbox', function(s) {
-        //$(this).html("<i class='material-icons'>check_box</i>") // Changes the icon to ticked.
         $(this).hide().html("<i class='material-icons'>check_box</i>").fadeIn('slow');
-        $(this).parent().css('color', 'grey');
+        $(this).parent().css('opacity', '0.5');
         var itemIndex = $(this).parent().index(); // Gets the number of the element in the task list on clicked.
         console.log(itemIndex);
         console.log("the item index is: " + taskArray[itemIndex]);
@@ -71,14 +98,52 @@ $(document).ready(function($) {
         });       
     });
 
-    $("#left-arrow").click(function() {
-
+    $leftArrow.on('click', function() {
+        if (page == undefined) {
+            page = 0;
+        }
+        switch (page) {
+            case -1:
+                page = 1;
+                SetThemeTwo();
+                break;
+            case 0:
+                page--;
+                SetThemeOne();
+                break;
+            case 1:
+                page--;
+                SetThemeDefault();
+                break;
+        }      
     });
 
-    $("#right-arrow").click(function() {
-
+    $rightArrow.on('click', function() {
+        if (page == undefined) {
+            page = 0;
+        }
+        switch (page) {
+            case -1:
+                page++;
+                SetThemeDefault();
+                break;
+            case 0:
+                page++
+                SetThemeTwo();
+                break;
+            case 1:
+                page = -1; // Allows looping.
+                SetThemeOne();
+                break;
+        }
     });
-  
+
+    $("#save").on('click', function() {
+        chrome.storage.sync.set({ 'pageNum': page }, function() {
+            alert("success!");
+        });
+    });
+
     $taskInput.bind('keypress', function(e) { // So the user can submit tasks by pressing enter.
         if (e.keyCode == 13) {
             var i = $taskInput.val();
